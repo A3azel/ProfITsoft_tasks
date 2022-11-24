411785024bcbe1404bf1498c13d86a83464e8504
Ihor Losinskyi <ihor1300@gmail.com>
2022-11-24 13:26:08 +0200

    Added buffer for second task

--- src/main/java/profITsoft/lectures3_4/task2/ViolationConvertor.java
+++ src/main/java/profITsoft/lectures3_4/task2/ViolationConvertor.java
@@ -1,8 +1,6 @@
 package profITsoft.lectures3_4.task2;
 
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -11,25 +9,27 @@ import profITsoft.lectures3_4.task2.entity.ViolationStatistics;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ViolationConvertor {
     private static final String DEFAULT_OUTPUT_XML_PATH = "src/main/resources/task2/outputFiles/";
     private static final String XML_SUFFIX = ".xml";
 
-    private static final String DATE_FORMAT="yyyy-MM-dd HH:mm:ss";
+    private static final String JSON_REGEX = "\\{\\s*[\\s\\S]+?}";
 
+    private static final String DATE_FORMAT="yyyy-MM-dd HH:mm:ss";
+    private static final int CUSTOM_JSON_BUFFER = 50;
+
+
+
+    // only 50 new json objects in memory
     public static void parseJSON(String dirPath, String outputXMLFileName){
         ObjectMapper mapper = new ObjectMapper();
         DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
@@ -45,21 +45,77 @@ public class ViolationConvertor {
         }
         for(File jsonFile: filesList){
             try(BufferedReader bufferedReader = new BufferedReader(new FileReader(jsonFile))){
-                List<Violation> violationList = mapper.readValue(bufferedReader, new TypeReference<>() {});
-                if(violationList!=null){
-                    allViolations.addAll(violationList);
+                StringBuilder jsonString = new StringBuilder();
+                String jsonLine;
+                int jsonCount = 0;
+                while((jsonLine=bufferedReader.readLine())!=null){
+                    jsonString.append(jsonLine).append("\n");
+                    Pattern pattern = Pattern.compile(JSON_REGEX);
+                    Matcher matcher = pattern.matcher(jsonString.toString());
+                    if (matcher.find()){
+                        Violation violation = mapper.readValue(matcher.group(),Violation.class);
+                        allViolations.add(violation);
+                        jsonCount++;
+                        if(jsonCount>CUSTOM_JSON_BUFFER){
+                            jsonCount = 0;
+                            List<Violation> statisticsList = creatingAndSortingStatistics(allViolations);
+                            allViolations.clear();
+                            allViolations.addAll(statisticsList);
+                        }
+                        jsonString.setLength(0);
+                    }
+                }
+                if(jsonCount!=0){
+                    List<Violation> statisticsList = creatingAndSortingStatistics(allViolations);
+                    allViolations.clear();
+                    allViolations.addAll(statisticsList);
                 }
             } catch (IOException fileNotFoundException) {
                 fileNotFoundException.printStackTrace();
             }
         }
-        List<Violation> statisticsList = creatingAndSortingStatistics(allViolations);
         try {
-            createXML(statisticsList,outputXMLFileName);
+            createXML(allViolations,outputXMLFileName);
         } catch (ParserConfigurationException | TransformerException e) {
             e.printStackTrace();
         }
     }
+
+    // entire file in memory
+    /*public static void parseJSON(String dirPath, String outputXMLFileName){
+            ObjectMapper mapper = new ObjectMapper();
+            DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+            mapper.setDateFormat(dateFormat);
+
+            List<Violation> allViolations = new ArrayList<>();
+
+            File jsonDirectory = new File(dirPath);
+            File[] filesList = jsonDirectory.listFiles();
+            if(filesList==null){
+                System.out.println("Selected directory is empty");
+                return;
+            }
+            for(File jsonFile: filesList){
+                try(BufferedReader bufferedReader = new BufferedReader(new FileReader(jsonFile))){
+                    List<Violation> violationList = mapper.readValue(bufferedReader, new TypeReference<>() {});
+                    if(violationList!=null){
+                        allViolations.addAll(violationList);
+                    }
+                    List<Violation> statisticsList = creatingAndSortingStatistics(allViolations);
+                    allViolations.clear();
+                    allViolations.addAll(statisticsList);
+                } catch (IOException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            }
+            //List<Violation> statisticsList = creatingAndSortingStatistics(allViolations);
+            try {
+                //createXML(statisticsList,outputXMLFileName);
+                createXML(allViolations,outputXMLFileName);
+            } catch (ParserConfigurationException | TransformerException e) {
+                e.printStackTrace();
+            }
+        }*/
 
     private static List<Violation> creatingAndSortingStatistics(List<Violation> violationList){
         return violationList.stream()
@@ -77,7 +133,6 @@ public class ViolationConvertor {
         XmlMapper mapper = new XmlMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         ViolationStatistics statistics = new ViolationStatistics(statisticsList);
-
         try {
             mapper.writeValue(new File(DEFAULT_OUTPUT_XML_PATH+outputXMLFileName+XML_SUFFIX), statistics);
         } catch (Exception e) {
